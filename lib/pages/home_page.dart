0797eba8ecc0e9f6fde7a6aad67f122c27eaad49
Karthick1304s <karthick.s1304@gmail.com>
2025-02-18 21:40:02 +0530

    all ready to implement chart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:chartapp/auth/auth_service.dart';
+import 'package:chartapp/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,20 +6,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    void logout() {
-      final _auth = AuthService();
-      _auth.signOut();
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
         backgroundColor: Colors.grey,
         centerTitle: true,
-        actions: [
-          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
-        ],
       ),
+      drawer: MyDrawer(),
     );
   }
 }
