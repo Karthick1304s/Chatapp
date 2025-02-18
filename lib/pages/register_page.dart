24d0b45a01550180f68c4c4df24dd434744a520f
Karthick1304s <karthick.s1304@gmail.com>
2025-02-18 17:10:45 +0530

    scroll in login and register

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -45,86 +45,88 @@ class RegisterPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Icon(
-              Icons.message,
-              size: 60,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            const SizedBox(height: 25),
-            //welcome back message
-            Text(
-              "Create an account",
-              style: TextStyle(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Icon(
+                Icons.message,
+                size: 60,
                 color: Theme.of(context).colorScheme.primary,
-                fontSize: 25,
               ),
-            ),
-            const SizedBox(height: 25),
-            //name
-            MyTextfield(
-              hintText: "Name",
-              hide: false,
-              controller: _nameController,
-            ),
-            const SizedBox(height: 20),
-            //email address
-            MyTextfield(
-              hintText: "Email",
-              hide: false,
-              controller: _emailController,
-            ),
-            const SizedBox(height: 20),
-            //password
-            MyTextfield(
-              hintText: "Password (6 char)",
-              hide: true,
-              controller: _passwordController,
-            ),
-            const SizedBox(height: 20),
-
-            //conform password
-            MyTextfield(
-              hintText: "Confirm Password",
-              hide: true,
-              controller: _cPasswordController,
-            ),
-            const SizedBox(height: 20),
-
-            //button
-            MyButton(
-              name: "Register",
-              onTap: () => register(context),
-            ),
-
-            const SizedBox(height: 25),
-
-            //register now
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have a account, ",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
+              const SizedBox(height: 25),
+              //welcome back message
+              Text(
+                "Create an account",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 25,
                 ),
-                GestureDetector(
-                  onTap: onTap,
-                  child: Text(
-                    "Login Now",
+              ),
+              const SizedBox(height: 25),
+              //name
+              MyTextfield(
+                hintText: "Name",
+                hide: false,
+                controller: _nameController,
+              ),
+              const SizedBox(height: 20),
+              //email address
+              MyTextfield(
+                hintText: "Email",
+                hide: false,
+                controller: _emailController,
+              ),
+              const SizedBox(height: 20),
+              //password
+              MyTextfield(
+                hintText: "Password (6 char)",
+                hide: true,
+                controller: _passwordController,
+              ),
+              const SizedBox(height: 20),
+
+              //conform password
+              MyTextfield(
+                hintText: "Confirm Password",
+                hide: true,
+                controller: _cPasswordController,
+              ),
+              const SizedBox(height: 20),
+
+              //button
+              MyButton(
+                name: "Register",
+                onTap: () => register(context),
+              ),
+
+              const SizedBox(height: 25),
+
+              //register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have a account, ",
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
                       color: Theme.of(context).colorScheme.primary,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      "Login Now",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
